2d50a6c75047f42fc4f8ac21635c018a9e4d39a1
kennyAnyi9 <kennyanyi@gmail.com>
2024-05-07 15:51:20 +0000

    modified stuff

--- AI-TOOLS/sel-bs.py
+++ AI-TOOLS/sel-bs.py
@@ -7,10 +7,10 @@ import json
 import datetime
 
 # Initialize WebDriver
-driver = webdriver.Firefox()  # Use the browser driver of your choice
+driver = webdriver.Firefox()  
 
 # Open the webpage
-driver.get('https://aitoptools.com/ai-tools/writing-content/')
+driver.get('https://allthingsai.com/category/chatbots')
 
 tool_list = []
 
@@ -18,18 +18,21 @@ while True:
     # Parse HTML and save to BeautifulSoup object
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
-    tools = soup.find_all('div', class_='custom-listing-content')
+    category = soup.find('div', class_='category-section-2')
+    tools = soup.find_all('div', class_='vertical-centered w-dyn-itemcategory-section-2')
+    
 
     for item in tools:
-        name = item.h2.text.strip()
+        name = item.h3.text.strip()
         purpose = item.p.text.strip()
-        current_date = datetime.date.today().strftime('%Y-%m-%d')  # Get today's date
-        tool_info = {"AI tool": name, "Purpose": purpose, "Scraped Date": current_date}
+        current_date = datetime.date.today().strftime('%Y-%m-%d')  
+        pricing_per_month = item.find('div', class_='text-size-regular text-color-black')
+        tool_info = {"AI tool": name, "Purpose": purpose, "Scraped Date": current_date, "monthly_price": pricing_per_month}
         tool_list.append(tool_info)
 
     try:
         # Try to click the 'next' button
-        next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#loadMore")))
+        next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#w-tabs-0-data-w-pane-0 > div > div.w-pagination-wrapper")))
         next_button.click()
     except Exception as e:
         # If 'next' button is not found, we're on the last page
@@ -41,8 +44,7 @@ driver.quit()
 # Convert data to JSON string
 json_data = json.dumps(tool_list)
 
-# Write JSON data to file (replace with your desired filename)
-with open('scraped_tools.json', 'w') as outfile:
+# Write JSON data to file (recplace with your desired filename)
+with open('chatbots.json', 'w') as outfile:
   outfile.write(json_data)
 
-print("Data scraped and saved to scraped_tools.json!")
